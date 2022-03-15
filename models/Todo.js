08336edcc533827eab6d11ea2bfe91626baf0350
Lucas Salomao <lucaas.afonso@hotmail.com>
2022-03-15 16:50:33 -0300

    Adding requirement on title field - Todo Model

--- models/Todo.js
+++ models/Todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema(
     {   
-        title: { type: String},
+        title: { type: String, required: true},
         completed: { type: Boolean, default: false}
     },    
     { 
